82c5ff60db5c639802a22c13e7830a2ce9d0ea39
Chris Gunn <cwize1@gmail.com>
2020-12-23 14:21:01 -0800

    Go: Don't import UUID package when it isn't used.

--- Tools/Go/test/bebopc_test.go
+++ Tools/Go/test/bebopc_test.go
@@ -1,13 +1,16 @@
 //go:generate dotnet run --project ../../../Compiler/ --config bebopConfig.json
+//go:generate dotnet run --project ../../../Compiler/ --files "../../../Laboratory/Schemas/array_of_strings.bop" --go generated/arrofstr/arrofstr.go --gopkg arrofstr
 
 package test
 
 import (
 	"testing"
 
-	"github.com/RainwayApp/bebop/Tools/Go/test/generated"
+	"github.com/RainwayApp/bebop/Tools/Go/test/generated/arrofstr"
+	"github.com/RainwayApp/bebop/Tools/Go/test/generated/other"
 )
 
 func TestBebopc(t *testing.T) {
-	_ = generated.Musician{}
+	_ = other.Musician{}
+	_ = arrofstr.ArrayOfStrings{}
 }
